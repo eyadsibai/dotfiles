5d6ec154d311b881b966e5d39e9af678f826a181
Eyad Sibai <eyad.alsibai@gmail.com>
2022-07-23 09:21:00 +0300

    update

--- overlays/whatsapp.nix
+++ overlays/whatsapp.nix
@@ -1,13 +1,22 @@
-self: super: {
-  whatsapp-for-linux = super.whatsapp-for-linux.overrideAttrs (drv: rec {
+final: prev: {
+  whatsapp-for-linux = prev.whatsapp-for-linux.overrideAttrs (_: rec {
     version = "1.4.5";
     pname = "whatsapp-for-linux";
-    src = super.fetchFromGitHub {
-    owner = "eneshecan";
-    repo = pname;
-    rev = "v${version}";
-    sha256 = "sha256-TX6fMuhe6VHbhWJSsPM0iOV4CuCfULD5McJyHuTW4lI=";
-  };
+    src = prev.fetchFromGitHub {
+      owner = "eneshecan";
+      repo = pname;
 
+      rev = "v${version}";
+      # sha256 = "0000000000000000000000000000000000000000000000000000";
+      sha256 = "V1QxPN99BAEnBvrfHyml0OsaS8msZXjkzvzItLqf8B0=";
+    };
+
+    buildInputs = [
+
+      prev.gtkmm3
+      prev.libayatana-appindicator
+      prev.webkitgtk
+    ];
   });
 }
+
