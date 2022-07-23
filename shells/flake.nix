5f6255c10083aa942ac154a55be9cfac821b5b49
Eyad Sibai <eyad.alsibai@gmail.com>
2022-07-23 10:47:14 +0300

    add ml shell

--- shells/flake.nix
+++ shells/flake.nix
@@ -19,6 +19,7 @@
           node = import ./node.nix { inherit pkgs; };
           python = import ./python.nix { inherit pkgs; };
           rust = import ./rust.nix { inherit pkgs; };
+          ml = import ./ml.nix { inherit pkgs; };
         };
       });
 
--- shells/ml.nix
+++ shells/ml.nix
@@ -2,6 +2,16 @@
 
 with pkgs;
 mkShell {
-  buildInputs = [ python310Packages.pip python310Packages.pip-tools ];
+  buildInputs = [
+    python310Packages.pip
+    python310Packages.pip-tools
+    opencv3
+    python310Packages.pandas
+    python310Packages.numpy
+    python310Packages.scipy
+    python310Packages.jupyter
+    # python310Packages.pyjanitor
+
+  ];
   shellHook = "";
 }
