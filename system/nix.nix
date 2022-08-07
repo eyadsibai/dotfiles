806e9f2df45f1fe680b0f3175b5f81060c440c32
Eyad Sibai <eyad.alsibai@gmail.com>
2022-08-07 13:52:14 +0300

    update

--- system/nix.nix
+++ system/nix.nix
@@ -14,6 +14,7 @@
 
     package = pkgs.nixFlakes;
 
+
     settings = {
       auto-optimise-store = true;
       extra-experimental-features = [ "nix-command" "flakes" ];
@@ -23,12 +24,15 @@
         "https://cache.nixos.org/"
         "https://nix-community.cachix.org"
         "https://shawn8901.cachix.org"
+        "https://eyadsibai.cachix.org"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "shawn8901.cachix.org-1:7RAYBGET4e+szLrg86T9PP1vwDp+T99Fq0sTDt3B2DA="
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "eyadsibai.cachix.org-1:7+k2Qd+uu7AGrS1AvO59mZJWn6PIvQAXK4EzAlqTSLA="
       ];
-
+		trusted-users = ["root" "eyad"];
     };
     daemonIOSchedClass = "idle";
     daemonCPUSchedPolicy = "idle";
