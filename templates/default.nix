75af805e3d6c882be51ce885fd0843535e86e025
Eyad Sibai <eyad.alsibai@gmail.com>
2022-08-13 01:04:55 +0300

    update

--- templates/default.nix
+++ templates/default.nix
@@ -2,10 +2,10 @@
   flake = {
     description = "basic flake environment template";
     path = ./flake;
-      };
+  };
 
-   flake-toml = {
-   	description = "flake envrionment supporting toml template";
-   	path = ./flake-toml;
-   }
-  }
+  flake-toml = {
+    description = "flake envrionment supporting toml template";
+    path = ./flake-toml;
+  };
+}
