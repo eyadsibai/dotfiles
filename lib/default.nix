0cb25d70f79cbc117f0790d3bfb871580d6a1d6b
Eyad Sibai <eyad.alsibai@gmail.com>
2022-08-18 15:11:50 +0300

    update

--- lib/default.nix
+++ lib/default.nix
@@ -1,3 +1,10 @@
-{ inputs, ... }: let inherit (inputs) self nixpkgs;inherit (self) outputs; in rec { }
+{ inputs, ... }:
+let
+  inherit (inputs) self home-manager nixpkgs;
+  inherit (self) outputs;
 
+  inherit (builtins) elemAt match any mapAttrs attrValues attrNames listToAttrs;
+  inherit (nixpkgs.lib) nixosSystem filterAttrs genAttrs mapAttrs';
+in
+rec { }
 #https://github.com/archseer/snowflake/blob/master/lib/utils.nix can I move mergeEnvs here?
