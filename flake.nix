fada016cc1f4355d9c8847aaa24006d57fa60bdb
Eyad Sibai <eyad.alsibai@gmail.com>
2022-08-21 14:39:51 +0300

    update

--- flake.nix
+++ flake.nix
@@ -84,128 +84,154 @@
         nur = inputs.nur.overlay;
         neovim = inputs.neovim-nightly-overlay.overlay;
         poetry2nix = inputs.poetry2nix.overlay;
-      };
-
-      nixos-overlays = { };
-
-      darwin-overlays = {
-        firefox-darwin = inputs.firefox-darwin.overlay;
-        spacebar = inputs.spacebar.overlay;
-      };
-
-      # Reusable nixos modules you might want to export
-      # These are usually stuff you would upstream into nixpkgs
-      nixosModules = import ./modules/nixos;
-      homeManagerModules = import ./modules/home-manager;
-      darwinModules = import ./modules/darwin;
-
-      templates = import ./templates;
-
-      # Reexport nixpkgs with our overlays applied
-      # Acessible on our configurations, and through nix build, shell, run, etc.
-      legacyPackages =
-        forAllSystems
-          (
-            system:
-            import
-              inputs.nixpkgs
+        bleeding-edge =
+          _final: _prev: {
+            bleeding-edge = import inputs.bleeding-edge
               {
                 inherit system;
-                overlays = builtins.attrValues common-overlays ++ [
-                  (_final: _prev: {
-                    bleeding-edge = import inputs.bleeding-edge
-                      {
-                        inherit system;
-                        config = nixConfig;
-                      };
-
-                    apple-x86_64 = import inputs.nixpkgs
-                      {
-                        system = "x86_64-darwin";
-                        config = nixConfig;
-                      };
-                  })
-                ] ++ (inputs.nixpkgs.lib.lists.optionals
-                  (builtins.elem
-                    system [ "aarch64-darwin" "x86_64-darwin" ])
-                  (builtins.attrValues
-                    darwin-overlays)) ++
-                  inputs.nixpkgs.lib.lists.singleton (
-                    # Sub in x86 version of packages that don't build on Apple Silicon yet
-                    final: prev: (inputs.nixpkgs.lib.optionalAttrs (prev.stdenv.system == "aarch64-darwin") {
-                      inherit (final.apple-x86_64)
-                        idris2
-                        # nix-index
-                        # niv
-                        # purescript
-                        ;
-                    })
-                  );
-
-
                 config = nixConfig;
               }
-          );
-      # Devshell for bootstrapping
-      # Acessible through 'nix develop'
-      devShells =
-        forAllSystems
-          (
-            system:
-            let
-              pkgs = legacyPackages.${ system };
-              mergeEnvs =
-                envs:
-                pkgs.mkShell
-                  (
-                    builtins.foldl'
-                      (
-                        a:
-                        v:
-                        {
-                          buildInputs = a.buildInputs ++ v.buildInputs;
-                          nativeBuildInputs = a.nativeBuildInputs ++ v.nativeBuildInputs;
-                          propagatedBuildInputs = a.propagatedBuildInputs ++ v.propagatedBuildInputs;
-                          propagatedNativeBuildInputs = a.propagatedNativeBuildInputs ++ v.propagatedNativeBuildInputs;
-                          shellHook = a.shellHook + "\n" + v.shellHook;
-                        }
-                      )
-                      (pkgs.mkShell { })
-                      envs
-                  );
-            in
-            rec {
-              default = pkgs.callPackage ./shell.nix { };
-              cc = pkgs.callPackage ./shells/cc.nix { };
-              go = pkgs.callPackage ./shells/go.nix { };
-              java = import ./shells/java.nix { inherit pkgs; };
-              # demonstrate two ways
-              node = import ./shells/node.nix { inherit pkgs; };
-              python = import ./shells/python.nix { inherit pkgs inputs; };
-              rust = import ./shells/rust.nix { inherit pkgs; };
-              ml = import ./shells/ml_no_cuda.nix { inherit pkgs; };
-              sys-stats = import ./shells/sys-stats.nix { inherit pkgs; };
-              db = import ./shells/db_dev.nix { inherit pkgs; };
-              r = import ./shells/r.nix { inherit pkgs; };
-              port-scanners = import ./shells/penetration/port-scanners.nix { inherit pkgs; };
-              load-testing = import ./shells/penetration/load-testing.nix { inherit pkgs; };
-              password = import ./shells/penetration/password.nix { inherit pkgs; };
-              mysql = import ./shells/mysql.nix { inherit pkgs; };
-              penetration-full = mergeEnvs [ port-scanners load-testing password ];
-            }
-          );
-      nixosConfigurations."eyad-nixos" =
-        inputs.nixpkgs.lib.nixosSystem
-          {
-            pkgs = legacyPackages.x86_64-linux;
-            modules =
-              [
-                ./hosts/eyad-nixos/nixos/configuration.nix
-                inputs.nixos-hardware.nixosModules.lenovo-thinkpad
-                inputs.nixpkgs.nixosModules.notDetected
-                inputs.nur.nixosModules.nur
-                # inputs.base16.nixosModules
-                inputs.home-manager.nixosModules.home-manager
+              };
+          };
+
+        nixos-overlays = { };
+
+        darwin-overlays = {
+          firefox-darwin = inputs.firefox-darwin.overlay;
+          spacebar = inputs.spacebar.overlay;
+          intel-overlay = self: super: (inputs.nixpkgs.lib.optionalAttrs
+            (super.stdenv.system == "aarch64-darwin")
+            {
+              inherit (import inputs.nixpkgs
+                {
+                  system = "x86_64-darwin";
+                  config = nixConfig;
+                })
+                idris2
+                # nix-index
+                ;
+            });
+        };
+
+        # Reusable nixos modules you might want to export
+        # These are usually stuff you would upstream into nixpkgs
+        nixosModules = import ./modules/nixos;
+        homeManagerModules = import ./modules/home-manager;
+        darwinModules = import ./modules/darwin;
+
+        templates = import ./templates;
+
+        # Reexport nixpkgs with our overlays applied
+        # Acessible on our configurations, and through nix build, shell, run, etc.
+        legacyPackages =
+          forAllSystems
+            (
+              system:
+              import
+                inputs.nixpkgs
+                {
+                  inherit system;
+                  overlays = builtins.attrValues common-overlays ++
+                    (inputs.nixpkgs.lib.lists.optionals
+                      (builtins.elem
+                        system [ "aarch64-darwin" "x86_64-darwin" ])
+                      (builtins.attrValues
+                        darwin-overlays));
+
+                  config = nixConfig;
+                }
+
+            );
+        # Devshell for bootstrapping
+        # Acessible through 'nix develop'
+        devShells =
+          forAllSystems
+            (
+              system:
+              let
+                pkgs = legacyPackages.${ system };
+                mergeEnvs =
+                  envs:
+                  pkgs.mkShell
+                    (
+                      builtins.foldl'
+                        (
+                          a:
+                          v:
+                          {
+                            buildInputs = a.buildInputs ++ v.buildInputs;
+                            nativeBuildInputs = a.nativeBuildInputs ++ v.nativeBuildInputs;
+                            propagatedBuildInputs = a.propagatedBuildInputs ++ v.propagatedBuildInputs;
+                            propagatedNativeBuildInputs = a.propagatedNativeBuildInputs ++ v.propagatedNativeBuildInputs;
+                            shellHook = a.shellHook + "\n" + v.shellHook;
+                          }
+                        )
+                        (pkgs.mkShell { })
+                        envs
+                    );
+              in
+              rec {
+                default = pkgs.callPackage ./shell.nix { };
+                cc = pkgs.callPackage ./shells/cc.nix { };
+                go = pkgs.callPackage ./shells/go.nix { };
+                java = import ./shells/java.nix { inherit pkgs; };
+                # demonstrate two ways
+                node = import ./shells/node.nix { inherit pkgs; };
+                python = import ./shells/python.nix { inherit pkgs inputs; };
+                rust = import ./shells/rust.nix { inherit pkgs; };
+                ml = import ./shells/ml_no_cuda.nix { inherit pkgs; };
+                sys-stats = import ./shells/sys-stats.nix { inherit pkgs; };
+                db = import ./shells/db_dev.nix { inherit pkgs; };
+                r = import ./shells/r.nix { inherit pkgs; };
+                port-scanners = import ./shells/penetration/port-scanners.nix { inherit pkgs; };
+                load-testing = import ./shells/penetration/load-testing.nix { inherit pkgs; };
+                password = import ./shells/penetration/password.nix { inherit pkgs; };
+                mysql = import ./shells/mysql.nix { inherit pkgs; };
+                penetration-full = mergeEnvs [ port-scanners load-testing password ];
+              }
+            );
+        nixosConfigurations."eyad-nixos" =
+          inputs.nixpkgs.lib.nixosSystem
+            {
+              pkgs = legacyPackages.x86_64-linux;
+              modules =
+                [
+                  ./hosts/eyad-nixos/nixos/configuration.nix
+                  inputs.nixos-hardware.nixosModules.lenovo-thinkpad
+                  inputs.nixpkgs.nixosModules.notDetected
+                  inputs.nur.nixosModules.nur
+                  # inputs.base16.nixosModules
+                  inputs.home-manager.nixosModules.home-manager
+                  {
+                    home-manager.useGlobalPkgs = true;
+                    home-manager.useUserPackages = true;
+                    home-manager.backupFileExtension = "backup";
+                    home-manager.extraSpecialArgs = { inherit inputs; };
+                    home-manager.users.eyad = {
+                      imports =
+                        [
+                          ./hosts/eyad-nixos/home-manager/home.nix
+
+                          # Import our reusable home-manager modules;
+                        ]
+                        ++ (builtins.attrValues homeManagerModules);
+                    };
+                  }
+                  # Import our reusable nixos modules;
+                ]
+                ++ (builtins.attrValues nixosModules);
+              specialArgs = { inherit inputs; };
+            };
+        darwinConfigurations."eyad-mac" =
+          inputs.darwin.lib.darwinSystem
+            {
+              system = "aarch64-darwin";
+              pkgs = legacyPackages.aarch64-darwin;
+              modules = [
+                # https://gitlab.com/azazel/ender-config/-/blob/master/flake.nix#L50
+                ./hosts/eyad-mac/darwin/configuration.nix
+
+                inputs.home-manager.darwinModules.home-manager
                 {
                   home-manager.useGlobalPkgs = true;
                   home-manager.useUserPackages = true;
@@ -214,49 +240,19 @@
                   home-manager.users.eyad = {
                     imports =
                       [
-                        ./hosts/eyad-nixos/home-manager/home.nix
+                        ./hosts/eyad-mac/home-manager/home.nix
 
                         # Import our reusable home-manager modules;
-                      ]
-                      ++ (builtins.attrValues homeManagerModules);
+
+                      ] ++ (builtins.attrValues homeManagerModules);
                   };
                 }
-                # Import our reusable nixos modules;
+
               ]
-              ++ (builtins.attrValues nixosModules);
-            specialArgs = { inherit inputs; };
-          };
-      darwinConfigurations."eyad-mac" =
-        inputs.darwin.lib.darwinSystem
-          {
-            system = "aarch64-darwin";
-            pkgs = legacyPackages.aarch64-darwin;
-            modules = [
-              # https://gitlab.com/azazel/ender-config/-/blob/master/flake.nix#L50
-              ./hosts/eyad-mac/darwin/configuration.nix
+              ++ (builtins.attrValues darwinModules);
 
-              inputs.home-manager.darwinModules.home-manager
-              {
-                home-manager.useGlobalPkgs = true;
-                home-manager.useUserPackages = true;
-                home-manager.backupFileExtension = "backup";
-                home-manager.extraSpecialArgs = { inherit inputs; };
-                home-manager.users.eyad = {
-                  imports =
-                    [
-                      ./hosts/eyad-mac/home-manager/home.nix
+              specialArgs = { inherit inputs; };
 
-                      # Import our reusable home-manager modules;
-
-                    ] ++ (builtins.attrValues homeManagerModules);
-                };
-              }
-
-            ]
-            ++ (builtins.attrValues darwinModules);
-
-            specialArgs = { inherit inputs; };
-
-          };
-    };
-}
+            };
+      };
+    }
