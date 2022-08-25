9e2ea5473db383b1721872b250f29ba9b476061c
Eyad Sibai <eyad.alsibai@gmail.com>
2022-08-25 13:37:34 +0300

    update

--- lib/default.nix
+++ lib/default.nix
@@ -1,10 +1,46 @@
 { inputs, ... }:
 let
-  inherit (inputs) self home-manager nixpkgs;
+  inherit (inputs) self home-manager;
   inherit (self) outputs;
 
   inherit (builtins) elemAt match any mapAttrs attrValues attrNames listToAttrs;
   inherit (nixpkgs.lib) nixosSystem filterAttrs genAttrs mapAttrs';
+  inherit (home-manager.lib) homeManagerConfiguration;
 in
-rec { }
+rec {
+  # Applies a function to a attrset's names, while keeping the values
+  mapAttrNames = f: mapAttrs' (name: value: { name = f name; inherit value; });
+
+
+  mkSystem =
+    { hostname
+    , pkgs
+    , persistence ? false
+    }:
+    nixosSystem {
+      inherit pkgs;
+      specialArgs = {
+        inherit inputs outputs hostname persistence;
+      };
+      modules = attrValues (import ../modules/nixos) ++ [ ../hosts/${hostname} ];
+    };
+
+  mkHome =
+    { username
+    , hostname ? null
+    , pkgs ? outputs.nixosConfigurations.${hostname}.pkgs
+    , persistence ? false
+    , colorscheme ? null
+    , wallpaper ? null
+    , features ? [ ]
+    }:
+    homeManagerConfiguration {
+      inherit pkgs;
+      extraSpecialArgs = {
+        inherit inputs outputs hostname username persistence
+          colorscheme wallpaper features;
+      };
+      modules = attrValues (import ../modules/home-manager) ++ [ ../home/${username} ];
+    };
+}
 #https://github.com/archseer/snowflake/blob/master/lib/utils.nix can I move mergeEnvs here?
