9f439a53e3ddb88eab1653e6651e0ab7252a5692
Eyad Sibai <eyad.alsibai@gmail.com>
2022-08-31 15:46:58 +0300

    add gtk

--- common/home-manager/gtk/default.nix
+++ common/home-manager/gtk/default.nix
@@ -1,59 +1,30 @@
-{ pkgs
-, ...
-}:
-{
-  home.packages = [ pkgs.nordic pkgs.arc-icon-theme pkgs.lxappearance ];
-  home.file.".gtkrc-2.0" = {
-    text =
-      ''
-        gtk-theme-name="Nordic-darker"
-        gtk-icon-theme-name="Arc"
-        gtk-font-name="Iosevka 11"
-        gtk-cursor-theme-size=0
-        gtk-toolbar-style=GTK_TOOLBAR_BOTH_HORIZ
-        gtk-toolbar-icon-size=GTK_ICON_SIZE_LARGE_TOOLBAR
-        gtk-button-images=0
-        gtk-menu-images=0
-        gtk-enable-event-sounds=1
-        gtk-enable-input-feedback-sounds=1
-        gtk-xft-antialias=1
-        gtk-xft-hinting=1
-        gtk-xft-hintstyle="hintmedium"
-      '';
+{ config, pkgs, inputs, ... }:
+
+let
+  inherit (inputs.nix-colors.lib-contrib { inherit pkgs; }) gtkThemeFromScheme;
+in
+rec {
+  gtk = {
+    enable = true;
+    font = {
+      name = config.fontProfiles.regular.family;
+      size = 12;
+    };
+    theme = {
+      name = "${config.colorscheme.slug}";
+      package = gtkThemeFromScheme { scheme = config.colorscheme; };
+    };
+    iconTheme = {
+      name = "Papirus";
+      package = pkgs.papirus-icon-theme;
+    };
   };
-  home.file.".config/gtk-2.0/gtkfilechooser.ini" = {
-    text =
-      ''
-        [Filechooser Settings]
-        LocationMode=path-bar
-        ShowHidden=false
-        ShowSizeColumn=true
-        GeometryX=442
-        GeometryY=212
-        GeometryWidth=1036
-        GeometryHeight=609
-        SortColumn=name
-        SortOrder=ascending
-        StartupMode=recent
-      '';
-  };
-  home.file.".config/gtk-3.0/settings.ini" = {
-    text =
-      ''
-        [Settings]
-        gtk-theme-name=Nordic-darker
-        gtk-icon-theme-name=Arc
-        gtk-font-name=Iosevka 11
-        gtk-cursor-theme-size=0
-        gtk-toolbar-style=GTK_TOOLBAR_BOTH_HORIZ
-        gtk-toolbar-icon-size=GTK_ICON_SIZE_LARGE_TOOLBAR
-        gtk-button-images=0
-        gtk-menu-images=0
-        gtk-enable-event-sounds=1
-        gtk-enable-input-feedback-sounds=1
-        gtk-xft-antialias=1
-        gtk-xft-hinting=1
-        gtk-xft-hintstyle=hintmedium
-      '';
+
+  services.xsettingsd = {
+    enable = true;
+    settings = {
+      "Net/ThemeName" = "${gtk.theme.name}";
+      "Net/IconThemeName" = "${gtk.iconTheme.name}";
+    };
   };
 }
