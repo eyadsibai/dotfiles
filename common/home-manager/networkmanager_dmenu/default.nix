c0cad88f2e6233b00e21a9096d5162cea71db34e
Eyad Sibai <eyad.alsibai@gmail.com>
2022-09-01 18:03:41 +0300

    cleanup apps

--- common/home-manager/networkmanager_dmenu/default.nix
+++ common/home-manager/networkmanager_dmenu/default.nix
@@ -2,22 +2,18 @@
 , pkgs
 , ...
 }:
-let
-  dmenu-command = "${ pkgs.rofi }/bin/rofi -dmenu";
-  default-terminal = "${ pkgs.kitty }/bin/kitty";
-in
 {
   home.packages = [ pkgs.networkmanager_dmenu ];
   xdg.configFile."networkmanager-dmenu/config.ini".text =
     ''
       [dmenu]
-      dmenu_command = ${dmenu-command}
+      dmenu_command = ${config.home.preferredApps.menu.dmenu-cmd}
       rofi_highlight = True
       wifi_chars = ▂▄▆█
       compact = True
 
       [editor]
-      terminal = ${default-terminal}
+      terminal = ${config.home.preferredApps.terminal.cmd}
 
     '';
 }
