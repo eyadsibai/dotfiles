1d589c408f9971b0269c24664c99ecceea1c5ba7
Eyad Sibai <eyad.alsibai@gmail.com>
2022-09-03 14:32:40 +0300

    update fonts

--- common/home-manager/polybar/default.nix
+++ common/home-manager/polybar/default.nix
@@ -1,7 +1,10 @@
 { pkgs
+, config
 , ...
 }:
 let
+
+  inherit (config.colorcheme) colors;
   ac = "#1E88E5";
   mf = "#383838";
   bg = "#00000000";
@@ -46,8 +49,8 @@ in
         background = bg;
         foreground = fg;
         radius = 0;
-        font-0 = "FuraCode Nerd Font:size=10;3";
-        font-1 = "FuraCode Nerd Font:style=Bold:size=10;3";
+        font-0 = "${config.fontProfiles.monospace.family}:size=10;3";
+        font-1 = "${config.fontProfiles.regular.family}:style=Bold:size=10;3";
         modules-left = "distro-icon dulS ddrT i3 dulT";
         modules-center = "title";
         modules-right = "durT audio ddlT wireless-network date";
@@ -71,8 +74,8 @@ in
         tray-padding = 5;
         tray-scale = 1;
         padding = 0;
-        font-0 = "FuraCode Nerd Font:size=10;3";
-        font-1 = "FuraCode Nerd Font:style=Bold:size=10;3";
+        font-0 = "${config.fontProfiles.regular.family}:size=10;3";
+        font-1 = "${config.fontProfiles.regular.family}:style=Bold:size=10;3";
         modules-left = "powermenu ddlS";
         modules-right = "ddrS cpu dulS ddrT memory dulT ddrP battery";
         locale = "en_US.UTF-8";
