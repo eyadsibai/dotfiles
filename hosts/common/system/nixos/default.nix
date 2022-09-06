c18f6ffa84477eee5b8f5f375603812473b7f866
Eyad Sibai <eyad.alsibai@gmail.com>
2022-09-06 17:35:38 +0200

    quick cleanup for fonts

--- hosts/common/system/nixos/default.nix
+++ hosts/common/system/nixos/default.nix
@@ -12,19 +12,17 @@ in
   imports = [ ../. ];
   services.locate.enable = true;
   services.geoclue2 = {
-    enable = true;
-    # {
-    # enable = lib.mkDefault
-    #   (
-    #     ((homeConfig.services.gammastep.enable or false) &&
-    #     (homeConfig.services.gammastep.provider == "geoclue2"))
-    #     || ((homeConfig.services.redshift.enable or false) &&
-    #     (homeConfig.services.redshift.provider == "geoclue2"))
-    #     || (config.services.localtimed.enable or false)
 
-    #   );
+    enable = lib.mkDefault
+      (
+        ((homeConfig.services.gammastep.enable or false) &&
+        (homeConfig.services.gammastep.provider == "geoclue2"))
+        || ((homeConfig.services.redshift.enable or false) &&
+        (homeConfig.services.redshift.provider == "geoclue2"))
+        || (config.services.localtimed.enable or false)
 
-    # enableWifi = true;
+      );
+    enableWifi = true;
   };
 
   services.tlp = {
