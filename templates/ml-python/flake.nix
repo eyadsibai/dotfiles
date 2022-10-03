6c53e219f124dcbb0e7fb0bdb667e97db5aa527c
Eyad Sibai <eyad.alsibai@gmail.com>
2022-10-03 21:10:52 +0300

    update flake.lock

--- templates/ml-python/flake.nix
+++ templates/ml-python/flake.nix
@@ -1,45 +1,26 @@
-let
-
-
-
-in
-pkgs.mkShell
-{
-  buildInputs = [
-    (pkgs.${python}.withPackages (ps: with ps; [ pip pyflakes isort ]))
-    #  pkgs.nodePackages.pyright
-    #  pkgs.nodePackages.prettier
-    #  pkgs.docker
-    #  pkgs.glpk
-    pythonShell
-    myPoetryEnv
-  ];
-  shellHook = "";
-}
-
-
-
-
 {
   inputs = {
-    utils.url = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
     dotfiles.url = "github:eyadsibai/dotfiles";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    mach-nix = "github:DavHau/mach-nix";
+    mach-nix.url = "github:DavHau/mach-nix";
 
   };
 
   outputs =
     { self
-    , utils
+    , flake-utils
     , dotfiles
     , nixpkgs
     , mach-nix
     }:
+    flake-utils.lib.eachDefaultSystem (system:
     let
       python = "python310";
 
-      mach-nix-wrapper = import inputs.mach-nix { inherit pkgs python; };
+      pkgs = nixpkgs.legacyPackages.${system};
+      custom_pkgs = dotfiles.legacyPackages.${system};
+      mach-nix-wrapper = import mach-nix { inherit pkgs python; };
       #  requirements = builtins.readFile ./requirements.txt;
       #  pythonShell = mach-nix-wrapper.mkPython { inherit requirements; };
       pythonShell-mach-nix =
@@ -56,42 +37,36 @@ pkgs.mkShell
         python = pkgs."${python}";
         preferWheels = true;
       };
-      out = system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          custom_pkgs = dotfiles.legacyPackages.${system};
-
-        in
-        rec {
-          devShell = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              poetry
-              custom_pkgs.rgf
-              custom_pkgs.fast-rgf
-              vowpal-wabbit
-              dvc
-              opencv
-              tesseract5
-              # tensorflow-lite
-              pre-commit
-              pythonShell-mach-nix
-              #            defaultPackage
-            ]
-              #             ++ [ myPoetryEnv]
-            ;
-          };
-
-          defaultPackage = with pkgs.poetry2nix;
-            mkPoetryApplication {
-              projectDir = ./.;
-              preferWheels = true;
-            };
-
-
-          defaultApp = utils.lib.mkApp {
-            drv = self.defaultPackage."${system}";
-          };
-        };
     in
-    with utils.lib; eachSystem defaultSystems out;
+    rec {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = [ pkgs.bashInteractive ];
+        buildInputs = with pkgs; [
+          poetry
+          custom_pkgs.rgf
+          custom_pkgs.fast-rgf
+          vowpal-wabbit
+          dvc
+          opencv
+          tesseract5
+          # tensorflow-lite
+          pre-commit
+          # pythonShell-mach-nix
+          #            defaultPackage
+        ]
+        ++ [ myPoetryEnv ]
+        ;
+      };
+
+      defaultPackage = with pkgs.poetry2nix;
+        mkPoetryApplication {
+          projectDir = ./.;
+          preferWheels = true;
+        };
+
+
+      # defaultApp = flake-utils.lib.mkApp {
+      #   drv = self.defaultPackage."${system}";
+      # };
+    });
 }
